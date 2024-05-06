837616d0403379e8159ee7cf78a4e9e652a8f8f8
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-05-05 20:10:38 -0400

    feat(pages): make pages about-us and cart client

--- app/pages/about-us/page.tsx
+++ app/pages/about-us/page.tsx
@@ -10,22 +10,61 @@ export default function ProductsPage() {
   const [selectedCity, setSelectedCity] = useState();
   const [categories, setCategories] = useState();
 
-  const filterCategories = (category: string) => {};
-  useEffect(() => {
-    fetch("https://662a68c267df268010a3d00f.mockapi.io/categories")
-      .then((res) => res.json())
-      .then((data) => {
-        setCategories(data);
-      });
-  }, []);
-
   return (
-    <div>
-      <div className="container m-5">
-        <div className="row">
-          <div className="col-12 mb-4">
-            <h1>About us</h1>
+    <div className="container m-5">
+      <div className="row">
+        <div className="col-12 mb-4">
+          <h1>About us</h1>
+        </div>
+      </div>
+
+      <div className="row m-5">
+        <div className="col-6">
+          <img src="/images/beneficios-trabajo-en-equipo.jpg" alt="" />
+        </div>
+
+        <div className="col-6 ">
+          <div className="row">
+            <div className="col-12 mb-3">
+              <h2>Notre mision</h2>
+            </div>
+
+            <div className="col-12 mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur, eos. Dicta velit quod natus, quaerat modi, quas
+                pariatur saepe tenetur qui ducimus laboriosam. Corporis facilis
+                eius sequi, deleniti rerum veritatis.
+              </p>
+            </div>
+
+            <div className="col-12 mb-3">
+              <h2>Organigrama</h2>
+            </div>
+
+            <div className="col-12">
+              <img src="/images/organigrama.png" alt="" />
+            </div>
           </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-12">
+          <h2>Notre products</h2>
+        </div>
+
+        <div className="col-12">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Temporibus, dignissimos possimus! Doloribus sit quam, fugit, cum
+            accusantium quidem fugiat id tenetur, nemo necessitatibus
+            repellendus a corporis dolorem repudiandae. Ullam, accusantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            beatae excepturi asperiores quaerat dolorem corporis blanditiis.
+            Eligendi cum aperiam animi quasi sed asperiores velit tenetur totam
+            porro blanditiis. Tempora, dolor.
+          </p>
         </div>
       </div>
     </div>
--- app/pages/cart/page.tsx
+++ app/pages/cart/page.tsx
@@ -1,11 +1,7 @@
 "use client";
+import { ProgressBar } from "primereact/progressbar";
 import React, { useEffect, useState } from "react";
-import { CardProduct } from "../../components/card-product/card-product";
-import { ImagesSlider } from "../../components/images-slider/images-slider";
-import ProductsList from "../../sections/products-list/products-list";
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
-import { FloatLabel } from "primereact/floatlabel";
+import { Button, Card, Form, InputGroup } from "react-bootstrap";
 export default function ProductsPage() {
   const [selectedCity, setSelectedCity] = useState();
   const [categories, setCategories] = useState();
@@ -14,8 +10,147 @@ export default function ProductsPage() {
     <div>
       <div className="container m-5">
         <div className="row">
-          <div className="col-12 mb-4">
-            <h1>Your Cart</h1>
+          <div className="col-6">
+            <div className="row">
+              <div className="col-12 mb-3">
+                <h1>Your Cart</h1>
+              </div>
+              <div className="col-12 mb-2">
+                <p style={{ textAlign: "center" }}>
+                  your order is eligeble for free shipping!
+                </p>
+              </div>
+
+              <div className="col-12 mb-3" style={{ textAlign: "center" }}>
+                <ProgressBar
+                  color="green"
+                  value={100}
+                  style={{ height: "10px", width: "90%", marginRight: "5%" }}
+                ></ProgressBar>
+              </div>
+
+              <div className="col-12 mb-3">
+                <Card style={{ borderRadius: "30px", height: "180px" }}>
+                  <div className="row" style={{ margin: "auto" }}>
+                    <div className="col-3">
+                      <img src="/images/camisa-real.jpg" alt="" />
+                    </div>
+
+                    <div className="col-6">
+                      <div className="row">
+                        <div className="col-12">
+                          <p>Men's Real Madrid 2023/24 Third Jersey</p>
+                        </div>
+
+                        <div className="col-12">
+                          <p>CA$140</p>
+                        </div>
+
+                        <div className="col-12">
+                          <p>
+                            <span>Size: </span>XL
+                          </p>
+                        </div>
+
+                        <div className="col-12">
+                          <InputGroup style={{ width: "120px" }}>
+                            <InputGroup.Text>-</InputGroup.Text>
+                            <Form.Control
+                              style={{ textAlign: "center" }}
+                              value={1}
+                              aria-label="coutProducts"
+                            />
+                            <InputGroup.Text>+</InputGroup.Text>
+                          </InputGroup>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-3">
+                      <p>CA$140</p>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+
+              <div className="col-12 mb-3">
+                <Card style={{ borderRadius: "30px", height: "180px" }}>
+                  <div className="row" style={{ margin: "auto" }}>
+                    <div className="col-3">
+                      <img src="/images/camisa-real.jpg" alt="" />
+                    </div>
+
+                    <div className="col-6">
+                      <div className="row">
+                        <div className="col-12">
+                          <p>Men's Real Madrid 2023/24 Third Jersey</p>
+                        </div>
+
+                        <div className="col-12">
+                          <p>CA$140</p>
+                        </div>
+
+                        <div className="col-12">
+                          <p>
+                            <span>Size: </span>XL
+                          </p>
+                        </div>
+
+                        <div className="col-12">
+                          <InputGroup style={{ width: "120px" }}>
+                            <InputGroup.Text>-</InputGroup.Text>
+                            <Form.Control
+                              style={{ textAlign: "center" }}
+                              value={1}
+                              aria-label="coutProducts"
+                            />
+                            <InputGroup.Text>+</InputGroup.Text>
+                          </InputGroup>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-3">
+                      <p>CA$140</p>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <div className="row" style={{ marginTop: "15%" }}>
+              <div className="col-12">
+                <Card style={{ height: "300px" }}>
+                  <div className="row" style={{ margin: "30px" }}>
+                    <div className="col-12">
+                      <h3>ORDER SUMMARY</h3>
+                    </div>
+                  </div>
+
+                  <div className="row" style={{ margin: "30px" }}>
+                    <div className="col-6">
+                      <p>SUBTOTAL: </p>
+                    </div>
+                    <div className="col-6">
+                      <p>CA$140</p>
+                    </div>
+
+                    <div className="col-12" style={{ marginTop: "12%" }}>
+                      <Button
+                        variant="success"
+                        style={{
+                          width: "100%",
+                        }}
+                      >
+                        CHECKOUT
+                      </Button>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
       </div>
