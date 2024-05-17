03148d4baa04b1f69843ab35a6a359cd127727c7
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-05-17 09:59:21 -0400

    feat(page): add new profile page

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -4,30 +4,74 @@ import { CardProduct } from "../../components/card-product/card-product";
 import { ImagesSlider } from "../../components/images-slider/images-slider";
 import ProductsList from "../../sections/products-list/products-list";
 import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
-import { FloatLabel } from "primereact/floatlabel";
+import { Toast, Tooltip } from "react-bootstrap";
+import { FileUpload } from "primereact/fileupload";
+
 export default function ProductsPage() {
-  const [selectedCity, setSelectedCity] = useState();
-  const [categories, setCategories] = useState();
-
   return (
-    <div>
-      <div className="container m-5">
-        <div className="row">
-          <div className="col-12 mb-4">
-            <h1>Hello! Name User</h1>
-          </div>
+    <div className="container m-5">
+      <div className="row">
+        <div className="col-12 mb-4">
+          <h1>Hello! Name User</h1>
         </div>
+      </div>
 
-        <div className="row">
+      <div className="row">
+        <div className="col-6">
+          <div className="col-12 mb-5">
+            <h3>You can update your information</h3>
+          </div>
           <div className="col-12">
-            <div className="flex flex-column gap-2">
-              <label htmlFor="username">Username</label>
-              <InputText id="username" aria-describedby="username-help" />
-              <small id="username-help">
-                Enter your username to reset your password.
-              </small>
-            </div>
+            <form action="">
+              <div>
+                <div className="flex flex-column gap-2 mb-4">
+                  <label htmlFor="username">Name</label>
+                  <InputText id="username" aria-describedby="username-help" />
+                  <small id="username-help">Enter your name.</small>
+                </div>
+
+                <div className="flex flex-column gap-2 mb-4">
+                  <label htmlFor="username">Last Name</label>
+                  <InputText id="username" aria-describedby="username-help" />
+                  <small id="username-help">Enter your last name.</small>
+                </div>
+
+                <div className="flex flex-column gap-2 mb-4">
+                  <label htmlFor="username">Password</label>
+                  <InputText
+                    id="username"
+                    aria-describedby="username-help"
+                    type="password"
+                  />
+                  <small id="username-help">Enter your new password.</small>
+                </div>
+
+                <div className="flex flex-column gap-2 mb-4 ">
+                  <label htmlFor="username">Address</label>
+                  <InputText
+                    id="username"
+                    aria-describedby="username-help"
+                    type="password"
+                    className="p-inputtext-lg"
+                  />
+                  <small id="username-help">Enter your new address.</small>
+                </div>
+
+                <div className="mb-5 col-12">
+                  <FileUpload
+                    name="demo[]"
+                    url="/api/upload"
+                    multiple
+                    accept="image/*"
+                    maxFileSize={1000000}
+                  />
+                </div>
+
+                <div>
+                  <button className="btn btn-success">Update Info</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
